65de1166faeca48498e4cb80f4a4f6d5a66cd0d3
ladronbx <ladronbravovlc@gmail.com>
2023-12-08 19:00:31 +0100

    [feat]: add Body.jsx with general structure to the Body folder in pages.

---
+++ proyecto-react/src/pages/Body/Body.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Home } from '../Home/Home';
+import { Login } from '../Login/Login';
+import { Register } from '../Register/Register';
+import { Profile } from '../Profile/Profile';
+
+export const Body = () => {
+     return (
+         <>
+            <Routes>
+                <Route path="*" element={<Navigate to="/"/>}/>
+                <Route path="/" element={<Home />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/register" element={<Register />}/>
+                <Route path="/profile" element={<Profile />}/>
+            </Routes>
+         </>
+     )
+}
---
+++ proyecto-react/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from React;
+import "./Home.css";
+
+export default function Home() {
+  return <div className="home-style">
+    <h1>Home</h1>
+    <p>Página principal!!!</p>
+  </div>;
+}
