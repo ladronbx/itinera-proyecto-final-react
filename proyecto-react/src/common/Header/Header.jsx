1fae0ce64a74941f097977828f8c32b2154eabb7
ladronbx <ladronbravovlc@gmail.com>
2023-12-09 07:42:02 +0100

    [feat] Add userSlice.js for Redux.

--- proyecto-react/src/common/Header/Header.jsx
+++ proyecto-react/src/common/Header/Header.jsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./Header.css";
 
 export const Header = () => {
-
-
     return (
-        <div className="navbar-header-style"> Soy el navbar
+    <div>Soy el header
         </div>
     );
 };
