420524f99b03c7460d55e028d513ceb979fc6408
ladronbx <ladronbravovlc@gmail.com>
2023-12-12 12:57:14 +0100

    [refactor]: modify tripSlice to handle 'tripId', 'location', and 'dates' information

--- src/pages/tripSlice.js
+++ src/pages/tripSlice.js
@@ -3,15 +3,25 @@ import { createSlice } from '@reduxjs/toolkit';
 export const tripSlice = createSlice({
     name: "trip",
     initialState: {
-        tripId: null
+      tripId: null,
+      location: null,
+      dates: null,
     },
     reducers: {
-        tripId: (state, action) => {
-            state.tripId = action.payload; 
-        },
-    }
-});
-
-export const selecttripId = (state) => state.trip.tripId;
-export const { tripId } = tripSlice.actions;
-export default tripSlice.reducer;
+      setTripId: (state, action) => {
+        state.tripId = action.payload;
+      },
+      setLocation: (state, action) => {
+        state.location = action.payload;
+      },
+      setDates: (state, action) => {
+        state.dates = action.payload;
+      },
+    },
+  });
+  
+  export const selectTripId = (state) => state.trip.tripId;
+  export const selectLocation = (state) => state.trip.location;
+  export const selectDates = (state) => state.trip.dates;
+  export const { setTripId, setLocation, setDates } = tripSlice.actions;
+  export default tripSlice.reducer;
