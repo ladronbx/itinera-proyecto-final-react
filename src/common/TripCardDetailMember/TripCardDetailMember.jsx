fca44cac0b80e4a4c35472ddc3ca7cd9fd66e1a4
ladronbx <ladronbravovlc@gmail.com>
2023-12-15 10:09:54 +0100

    [feat]: display trip members information as cards in TripDetail view using TripCardDetailMember component

---
+++ src/common/TripCardDetailMember/TripCardDetailMember.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TripCardDetailMember.css";
+
+export const TripCardDetailMember = ({ name, image, email }) => {
+    let roleText = "";
+
+    return (
+        <div className="card-allusers row">
+            <div className="card-alluser-container-image">
+                <img className="card-allusers-image" src={image} alt={name} />
+            </div>
+
+            <div className="card-allusers__content col">
+                <p className="card-allusers__title">{name}</p>
+                <p className="card-allusers__description">{email}</p>
+            </div>
+        </div>
+    );
+};
--- src/pages/TripDetail/TripDetail.jsx
+++ src/pages/TripDetail/TripDetail.jsx
@@ -4,6 +4,7 @@ import { selectToken } from '../userSlice';
 import { useSelector } from 'react-redux';
 import { getMyTripById } from "../../services/apiCall";
 import { useParams } from "react-router-dom";
+import { TripCardDetailMember } from "../../common/TripCardDetailMember/TripCardDetailMember";
 
 export const TripDetail = () => {
     const rdxToken = useSelector(selectToken);
@@ -27,7 +28,7 @@ export const TripDetail = () => {
     }, [id, rdxToken]);
 
     if (!trip) {
-        return <div>Cargando...</div>;
+        return <div>Loading...</div>;
     }
 
     return (
@@ -36,12 +37,22 @@ export const TripDetail = () => {
             <p>Fecha de inicio: {new Date(trip.start_date).toLocaleDateString()} - Fecha de fin: {new Date(trip.end_date).toLocaleDateString()}</p>
             <p>Número de viajeros: {trip.members_group}</p>
             <h2>Miembros del grupo:</h2>
-            {trip.members_name.map((member, index) => (
-                <div key={index}>
-                    <p>Nombre: {member}</p>
-                    <p>Email: {trip.members_email[index]}</p>
-                </div>
-            ))}
+            {
+                trip.members_name.length > 0
+                ? (
+                    trip.members_name.map((name, index) => (
+                        <TripCardDetailMember
+                            key={index}
+                            name={name}
+                            email={trip.members_email[index]}
+                            image={trip.members_image[index]}
+                        />
+                    ))
+                )
+                : (
+                    <div>No hay miembros en el grupo</div>
+                )
+            }
             <h2>Actividades:</h2>
             {trip.activity_name.map((activity, index) => (
                 <div key={index}>
