58159b633ca46891dd77b186b81b7bdd3e3df63a
ladronbx <ladronbravovlc@gmail.com>
2023-12-20 19:27:06 +0100

    [feat]: separate responsibility for updating password into PasswordModal component

---
+++ src/common/PasswordModal/PasswordModal.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { Modal, Form, Input, Button, Alert } from 'antd';
+import { updatePassword, getProfile } from '../../services/apiCall';
+
+function PasswordModal({ isOpen, onClose, rdxToken }) {
+  const [errorMessages, setErrorMessages] = useState({
+    password: "",
+  });
+
+  const [password, setPassword] = useState({
+    currentPassword: "",
+    newPassword: "",
+  });
+
+  const handleUpdatePassword = (event) => {
+    const { name, value } = event.target;
+    setPassword({ ...password, [name]: value });
+  }
+
+  const handleUpdatePasswordClick = () => {
+    updatePassword(password, rdxToken)
+      .then((response) => {
+        if (response.data.success) {
+          console.log(response.data);
+          getProfile(rdxToken)
+            .then((response) => {
+              console.log(response.data);
+              onClose();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        } else {
+          console.log(response.data);
+          setErrorMessages({ password: response.data.message });
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setErrorMessages({ password: "Asegúrate de que la contraseña antigua sea correcta. Recuerda que la nueva contraseña debe tener entre 6 y 12 carácteres e incluir al menos un símbolo" });
+      });
+  };
+
+  return (
+    <Modal
+      title="Update Password"
+      visible={isOpen}
+      onCancel={onClose}
+      footer={null}
+    >
+      <Form onFinish={handleUpdatePasswordClick}>
+        <Form.Item>
+          <Input.Password
+            name="currentPassword"
+            value={password.currentPassword}
+            onChange={handleUpdatePassword}
+            placeholder="Old password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input.Password
+            name="newPassword"
+            value={password.newPassword}
+            onChange={handleUpdatePassword}
+            placeholder="New password"
+          />
+        </Form.Item>
+        {errorMessages && errorMessages.password && (
+          <Form.Item>
+            <Alert type="error" message={errorMessages.password} />
+          </Form.Item>
+        )}
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Update Password
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
+
+export default PasswordModal;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { selectToken } from "../userSlice";
 import { getProfile, updateProfile, updatePassword } from "../../services/apiCall";
+import PasswordModal from "../../common/PasswordModal/PasswordModal";
 
 export const Profile = () => {
     const rdxToken = useSelector(selectToken);
@@ -17,8 +18,6 @@ export const Profile = () => {
         nameError: "",
         emailError: "",
         imageError: "",
-        currentPassword: "",
-        newPassword: "",
     });
 
     const [password, setPassword] = useState({
@@ -27,6 +26,8 @@ export const Profile = () => {
     });
 
     const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editingField, setEditingField] = useState(null);
 
     useEffect(() => {
         if (rdxToken) {
@@ -42,8 +43,6 @@ export const Profile = () => {
         }
     }, [rdxToken, navigate]);
 
-    const [editingField, setEditingField] = useState(null);
-
     const handleEditClick = (fieldName) => {
         setEditingField(fieldName);
     };
@@ -53,21 +52,13 @@ export const Profile = () => {
         setUser({ ...user, [name]: value });
     };
 
-    //handleUpdatePassword
-    const handleUpdatePassword = (event) => {
-        const { name, value } = event.target;
-        setPassword({ ...password, [name]: value });
-    }
-
     const handleUpdateProfile = () => {
         const updatedUser = { [editingField]: user[editingField] };
-        //si los campos son name, email o image = updateProfile
         if (editingField === 'name' || editingField === 'email' || editingField === 'image') {
             updateProfile(updatedUser, rdxToken)
                 .then((response) => {
                     console.log(response.data);
                     setEditingField(null);
-
                     getProfile(rdxToken)
                         .then((response) => {
                             setUser(response.data.data);
@@ -75,39 +66,12 @@ export const Profile = () => {
                         .catch((error) => {
                             console.log(error);
                         });
-
                 })
                 .catch((error) => {
                     console.log(error.response.data);
                     setErrorMessages(error.response.data.error);
                 });
         }
-    };
-
-    const handleUpdatePasswordClick = () => {
-        updatePassword(password, rdxToken)
-            .then((response) => {
-                if (response.data.success) {
-                    console.log(response.data);
-                    setEditingField(null);
-                    getProfile(rdxToken)
-                        .then((response) => {
-                            setUser(response.data.data);
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                        });
-
-                } else {
-                    console.log(response.data);
-                    setErrorMessages({ password: response.data.message });
-
-                }
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setErrorMessages({ password: "Asegúrate de que la contraseña antigua sea correcta. Recuerda que la nueva contraseña debe tener entre 6 y 12 carácteres e incluir al menos un símbolo" });
-            });
     };
 
     return (
@@ -166,27 +130,13 @@ export const Profile = () => {
                                     <button className="button-update-profile" onClick={handleUpdateProfile}>Update my profile</button>
                                 </div>
 
-
-                                <div className="password-profile">
-                                    {editingField === 'password'
-                                        ? <div>
-                                            <input className="password-profile-input" type="password" name="currentPassword" placeholder="Current Password" value={password.currentPassword} onChange={handleUpdatePassword} />
-                                            <input className="password-profile-input" type="password" name="newPassword" placeholder="New Password" value={password.newPassword} onChange={handleUpdatePassword} />
-                                            {errorMessages && errorMessages.password && (
-                                                <div className="error-message-style">
-                                                    {errorMessages.password}
-                                                </div>
-                                            )}
-                                            <button className="button-update-password" onClick={handleUpdatePasswordClick}>Update Password<svg height="1em" viewBox="0 0 512 512">
-                                                <path
-                                                    d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-                                                ></path>
-                                            </svg></button>
-                                        </div>
-                                        : <>
-                                            <button className="button-edit" onClick={() => handleEditClick('password')}>Change Password</button>
-                                        </>
-                                    }
+                                <div>
+                                    <button onClick={() => setIsModalOpen(true)}>Change Password</button>
+                                    <PasswordModal
+                                        isOpen={isModalOpen}
+                                        onClose={() => setIsModalOpen(false)}
+                                        rdxToken={rdxToken}
+                                    />
                                 </div>
 
 
