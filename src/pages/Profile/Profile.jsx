3cf1c981d5dc44874583f0d116dfbd067c946062
ladronbx <ladronbravovlc@gmail.com>
2023-12-24 13:05:04 +0100

    [style]: add styles for profile and home.

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -111,70 +111,76 @@ export const Profile = () => {
             {
                 user
                     ? (
-                        <div className="container-profile">
-
-
-                            <div className="container-left-img-profile col">
-                                <img src={user.image} alt="User" className="profile-image" />
+                        <>
+                            <div className="container-profile-basic">
+                                <h1 className="saludo-profile">Hola {user.name}</h1>
                             </div>
 
-                            <div className="profile-column col">
+                            <div className="container-profile">
 
-                                <div className={`name-profile ${isNameButtonClicked ? '' : 'name-profile-click'}`}>
-                                    {editingField === 'name'
-                                        ? <div>
-                                            <input className="name-profile-input" type="text" name="name" value={user.name} onChange={handleInputChange} />
-                                            {errorMessages.name && <div className="error-message-style">{errorMessages.name}</div>}
-                                        </div>
-                                        : <>
-                                            {user.name}
-                                            <button className="button-edit" onClick={() => handleEditClick('name')}>
-                                                <svg height="1em" viewBox="0 0 512 512">
-                                                    <path
-                                                        d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-                                                    ></path>
-                                                </svg>
-                                            </button>
-                                        </>
-                                    }
+                                <div className="container-left-img-profile col">
+                                    <img src={user.image} alt="User" className="profile-image" />
                                 </div>
 
-                                <div className={`email-name-profile ${isEmailButtonClicked ? '' : 'email-name-profile-click'}`}>
-                                    {editingField === 'email'
-                                        ? <div>
-                                            <input className="email-profile-input" type="email" name="email" value={user.email} onChange={handleInputChange} />
-                                            {errorMessages.email && <div className="error-message-style">{errorMessages.email}</div>}
-                                        </div>
-                                        : <>
-                                            {user.email}
-                                            <button className="button-edit" onClick={() => handleEditClick('email')}>
-                                                <svg height="1em" viewBox="0 0 512 512">
-                                                    <path
-                                                        d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-                                                    ></path>
-                                                </svg>
-                                            </button>
-                                        </>
-                                    }
+                                <div className="profile-column col">
+
+                                    <div className={`name-profile ${isNameButtonClicked ? '' : 'name-profile-click'}`}>
+                                        {editingField === 'name'
+                                            ? <div>
+                                                <input className="name-profile-input" type="text" name="name" value={user.name} onChange={handleInputChange} />
+                                                {errorMessages.name && <div className="error-message-style">{errorMessages.name}</div>}
+                                            </div>
+                                            : <>
+                                                {user.name}
+                                                <button className="button-edit" onClick={() => handleEditClick('name')}>
+                                                    <svg height="1em" viewBox="0 0 512 512">
+                                                        <path
+                                                            d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
+                                                        ></path>
+                                                    </svg>
+                                                </button>
+                                            </>
+                                        }
+                                    </div>
+
+                                    <div className={`email-name-profile ${isEmailButtonClicked ? '' : 'email-name-profile-click'}`}>
+                                        {editingField === 'email'
+                                            ? <div>
+                                                <input className="email-profile-input" type="email" name="email" value={user.email} onChange={handleInputChange} />
+                                                {errorMessages.email && <div className="error-message-style">{errorMessages.email}</div>}
+                                            </div>
+                                            : <>
+                                                {user.email}
+                                                <button className="button-edit" onClick={() => handleEditClick('email')}>
+                                                    <svg height="1em" viewBox="0 0 512 512">
+                                                        <path
+                                                            d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
+                                                        ></path>
+                                                    </svg>
+                                                </button>
+                                            </>
+                                        }
+                                    </div>
+
+                                    <div className="button-update-profile-container">
+                                        <button className="button-update-profile" onClick={handleUpdateProfile}>Update my profile</button>
+                                    </div>
+
+                                    <div>
+                                        <button className="change-password-style" onClick={() => setIsModalOpen(true)}>Cambiar contraseña</button>
+                                        <PasswordModal
+                                            isOpen={isModalOpen}
+                                            onClose={() => setIsModalOpen(false)}
+                                            rdxToken={rdxToken}
+                                        />
+                                    </div>
+
+                                    <button className="delete-count-style" onClick={handleDeleteAccount}>Eliminar cuenta</button>
+
                                 </div>
-
-                                <div className="button-update-profile-container">
-                                    <button className="button-update-profile" onClick={handleUpdateProfile}>Update my profile</button>
-                                </div>
-
-                                <div>
-                                    <button className="change-password-style" onClick={() => setIsModalOpen(true)}>Cambiar contraseña</button>
-                                    <PasswordModal
-                                        isOpen={isModalOpen}
-                                        onClose={() => setIsModalOpen(false)}
-                                        rdxToken={rdxToken}
-                                    />
-                                </div>
-
-                                <button className="delete-count-style" onClick={handleDeleteAccount}>Eliminar cuenta</button>
-
                             </div>
-                        </div>
+                        </>
+
                     )
                     : (
                         <div>Loading ...</div>
